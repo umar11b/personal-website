b950e3bf8ceadd3d7f65b48990005c8fc5cd1dde
Umar Zaman <uzaman2018@gmail.com>
2024-08-15 11:00:30 -0400

    navbar/navlink initialized

---
+++ src/app/components/NavBar.jsx
@@ -0,0 +1,44 @@
+import Link from "next/link";
+import React from "react";
+import NavLink from "./NavLink";
+
+const NavLink = [
+  {
+    title: "About",
+    href: "#about",
+  },
+  {
+    title: "Projects",
+    href: "#projects",
+  },
+  {
+    title: "Contact",
+    href: "#contact",
+  },
+];
+
+const NavBar = () => {
+  return (
+    <nav>
+      <div className="flex flex-wrap items-center justify-between mx-auto p-8">
+        <Link href={"/"} className="text-5xl text-white font-semibold">
+          LOGO
+        </Link>
+        <div className="menu hidden md:block md:w-auto" id="navbar">
+          <ul>
+            <li>
+              <Link
+                href={"#about"}
+                className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white"
+              >
+                About
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import { Container } from "postcss";
 import HeroSection from "./components/HeroSection,";
+import NavBar from "./components/NavBar";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212] container mx-auto px-12 py-6">
+      <NavBar />
       <div className="container mx-auto px-12 py-4">
         <HeroSection />
       </div>
