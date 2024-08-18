a6efaab079c8de6d4dbb9c2bd797afe47b2c68a6
Umar Zaman <uzaman2018@gmail.com>
2024-08-18 01:08:38 -0400

    updated navbar

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -1,8 +1,10 @@
+"use client";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import NavLink from "./NavLink";
+import { Bars3Icon, XmarkIcon } from "@heroicons/react/24/solid";
 
-const NavLink = [
+const navLinks = [
   {
     title: "About",
     href: "#about",
@@ -18,22 +20,43 @@ const NavLink = [
 ];
 
 const NavBar = () => {
+  const [navbarisOpen, setNavbarOpen] = useState(false);
+
   return (
-    <nav>
-      <div className="flex flex-wrap items-center justify-between mx-auto p-8">
-        <Link href={"/"} className="text-5xl text-white font-semibold">
+    <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90">
+      <div className="flex flex-wrap items-center justify-between mx-auto px-4 py-2">
+        <Link
+          href={"/"}
+          className="text-xl md:text-5xl text-white font-semibold"
+        >
           LOGO
         </Link>
+        <div className="mobile-menu block md:hidden">
+          {!navbarisOpen ? (
+            <button
+              onClick={() => setNavbarOpen(true)}
+              className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
+            >
+              <Bars3Icon className="h-5 w-5" />
+            </button>
+          ) : (
+            <button
+              onClick={() => setNavbarOpen(false)}
+              className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
+            >
+              <XmarkIcon className="h-5 w-5" />
+            </button>
+          )}
+        </div>
         <div className="menu hidden md:block md:w-auto" id="navbar">
-          <ul>
-            <li>
-              <Link
-                href={"#about"}
-                className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white"
-              >
-                About
-              </Link>
-            </li>
+          <ul className="flex p-4 md:p-0 sm:flex-row md:space-x-8 mt-0">
+            {navLinks.map((link, index) =>
+              link.path ? (
+                <li key={index}>
+                  <NavLink href={link.path} title={link.title} />
+                </li>
+              ) : null
+            )}
           </ul>
         </div>
       </div>
